d1ad46307f57e5992345bc2aeb919efa2db540c1
David Garcia <david.garcia6@utp.ac.pa>
2025-09-18 21:57:57 -0500

    Laboratorio 41

---
+++ Laboratorio 4/laboratorio43/Program.cs
@@ -0,0 +1,28 @@
+﻿static void Main(string[] args)
+{
+    int suma, cant, valor, promedio;
+    string linea;
+    suma = 0;
+    cant = 0;
+    do
+    {
+        Console.Write("Ingrese un numero (0 para finalizar): ");
+        linea = Console.ReadLine();
+        if (valor != 0)
+        {
+            suma=suma + valor;
+            cant=cant ++;
+        }
+    } while (valor != 0);
+    if (cant != 0)
+    {
+        promedio = suma / cant;
+        Console.Write("El promedio de los valores es:");
+        Console.Write(promedio);
+    }
+    else
+    {
+        Console.Write("No se ingresaron valores.");
+    }
+    Console.ReadLine();
+}
