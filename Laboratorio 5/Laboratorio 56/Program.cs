2210b9fedd62c0c43e6b7f21f7cee6a09a379068
David Garcia <david.garcia6@utp.ac.pa>
2025-09-23 19:52:31 -0500

    Laboratorio 5 completado

---
+++ Laboratorio 5/Laboratorio 56/Program.cs
@@ -0,0 +1,11 @@
+﻿Dictionary<String, string> paisesYCapitales = new Dictionary<String, string>();
+{
+    { "Francia", "Paris"},
+    { "España", "Madrid"},
+    { "Italia", "Roma"}
+};
+
+foreach (KeyValuePair<string, string> par in paisesYCapitales)
+{
+    Console.WriteLine("La capital de " + par.Key + " es " + par.Value + ".");
+}
