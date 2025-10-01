22ca601b75055924c068f507f5c459a62038df08
David Garcia <david.garcia6@utp.ac.pa>
2025-09-30 20:11:02 -0500

    Laboratorio 7 Completado

--- Laboratorio 7/Laboratorio71/obj/Release/net8.0/Laboratorio71.AssemblyInfo.cs
+++ Laboratorio 7/Laboratorio71/obj/Release/net8.0/Laboratorio71.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio71")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77276b3e8ecc28121455d8215795139ee080924a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+740d876863cbcfb966f913dde7e2d40eed0532df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio71")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio71")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
