0935c5a4657ec8469c7ac36b6af7fb6c782ff037
David Garcia <david.garcia6@utp.ac.pa>
2025-12-04 21:13:00 -0500

    Parcial 3 completado

--- Parcial 3/Controllers/HomeController.cs
+++
@@ -1,8 +0,0 @@
-
-using Microsoft.AspNetCore.Mvc;
-
-namespace OficinaPasaportesWeb.Controllers {
- public class HomeController:Controller{
-  public IActionResult Index()=>View();
- }
-}
--- Parcial 3/Models/RequestStatus.cs
+++
@@ -1,7 +0,0 @@
-
-namespace OficinaPasaportesWeb.Models {
- public class RequestStatus {
-  public int EstadoId{get;set;}
-  public string EstadoNombre{get;set;}
- }
-}
